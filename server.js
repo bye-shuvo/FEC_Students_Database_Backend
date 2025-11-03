ae502959f737af551f38b7d27d4f47daa09464fa
bye-shuvo <abuhasnatshuvo605@gmail.com>
2025-11-04 01:54:07 +0600

    Update server.js

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const PORT = process.env.PORT;
 const studentRoute = require("./database/students.js");
 app.use(cors({origin : ["https://fecsdb.vercel.app"]}));
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 app.listen(PORT , () => {
   console.log(`Server started at PORT : ${PORT}`);
 })
 
 app.get("/" , (req ,res)=>{
-  res.send("<h1 style='position:absolute; top:50%; left:50%; transform:translate(-50% , -50%); font-size:4rem;'>Welcome To FEC Students Portal</h1>");
+  res.send("<h1 style='position:absolute; top:50%; left:50%; transform:translate(-50% , -50%); font-size:4rem; text:center;'>Welcome To FEC Students Portal</h1>");
 })
 
 app.use("/students" , studentRoute);
